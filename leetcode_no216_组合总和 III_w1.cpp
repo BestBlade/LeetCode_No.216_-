b70ef2009a4749dc6501bcaf5ecaae3bc2c00fbd
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-26 15:29:54 +0800

    Add files via upload

---
+++ leetcode_no216_组合总和 III_w1.cpp
@@ -0,0 +1,94 @@
+﻿/* ------------------------------------------------------------------|
+找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+
+说明：
+
+所有数字都是正整数。
+解集不能包含重复的组合。 
+示例 1:
+
+输入: k = 3, n = 7
+输出: [[1,2,4]]
+示例 2:
+
+输入: k = 3, n = 9
+输出: [[1,2,6], [1,3,5], [2,3,4]]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combination-sum-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	dfs法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.6 MB,, 在所有 C++ 提交中击败了52.61%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<vector<int>> res;
+vector<int> tmp;
+int sum = 0;
+
+void dfs(int curr,int k, int n) {
+    if (tmp.size() == k && sum == n) {
+        res.emplace_back(tmp);
+        return;
+    }
+
+    if (tmp.size() + (9 - curr + 1) < k || tmp.size() > k) {
+        return;
+    }
+
+    tmp.emplace_back(curr);
+    sum += curr;
+    dfs(curr + 1, k, n);
+
+    sum -= curr;
+    tmp.pop_back();
+    dfs(curr + 1, k, n);
+}
+
+vector<vector<int>> combinationSum3(int k, int n) {
+    dfs(1, k, n);
+    return res;
+}
